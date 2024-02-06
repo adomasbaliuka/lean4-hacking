e80d7aab5e768217f804b2e0b10c18cedfe43c00
Adomas Baliuka <A.Baliuka@physik.uni-muenchen.de>
2024-02-06 15:59:17 +0100

    Adds lean4checker step to CI. won't work yet

--- Lean4Hacking/StatementOfFalse.lean
+++ Lean4Hacking/StatementOfFalse.lean
@@ -3,8 +3,8 @@ Copyright (c) 2024 Adomas Baliuka. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Adomas Baliuka
 -/
-import Mathlib
+-- import Mathlib
 import Lean4Hacking.ProofOfFalse
 
-lemma statement_of_false : False := by
+theorem statement_of_false : False := by
   sorry
